a4506786b5d32d486c156139f10ffcfc8df42f9f
Tchewi <quentin.leray@heig-vd.ch>
2019-04-05 10:57:27 +0200

    Modification pour tests en C

---
+++ Tests/main.c
@@ -0,0 +1,37 @@
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : Tests
+Fichier     : main.cpp
+Auteur(s)   : Quentin Le Ray
+Date        : 10.01.2019
+
+But         : Supprimer les doublons d'un vecteur
+
+Remarque(s) :
+
+Compilateur : MinGW-g++ <6.3.0>
+-----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+
+//#include <iostream>
+//#include <iomanip>
+//#include <vector>
+//#include <string>
+//#include <cstdlib> 
+//#include <algorithm>
+
+
+int main() {
+	
+
+	int test = 45;
+
+	printf("Hello %i \n", test);
+
+	
+	system("PAUSE");
+	return 0;
+
+}
